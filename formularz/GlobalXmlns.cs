79c95146c5db374cce97b3c99849cbc7ac814cd9
AshinaGenichiro <uT_4011@zsp3zamosc.pl>
2026-01-13 14:33:37 +0100

    zad1

---
+++ formularz/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "formularz")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "formularz.Pages")]
