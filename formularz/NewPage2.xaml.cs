42a988bf1cd8c180116dfc378134a67a6cef74f9
AshinaGenichiro <uT_4011@zsp3zamosc.pl>
2026-01-26 13:21:46 +0100

    zadanie 17.4

--- formularz/NewPage2.xaml.cs
+++ formularz/NewPage2.xaml.cs
@@ -6,4 +6,16 @@ public partial class NewPage2 : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void alert(object sender, EventArgs e)
+    {
+        var thumbnai = sender as Image;
+
+        if (thumbnai != null)
+        {
+            await DisplayAlert(
+                "Miniatura",
+                "Kliknieto w miniature w galerii",
+                "OK");
+        }
+    }
 }
